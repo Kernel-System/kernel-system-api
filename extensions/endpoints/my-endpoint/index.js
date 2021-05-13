27269fc7b5081ab130a24134d0825a77d5d5fcc0
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-05-13 12:25:34 -0600

    refactor(my-endpoint): ejemplo my end-point

--- extensions/endpoints/my-endpoint/index.js
+++ extensions/endpoints/my-endpoint/index.js
@@ -1,3 +1,5 @@
 module.exports = function registerEndpoint(router) {
-  router.get('/', (req, res) => res.send('Hello, World!'))
-}
+  router.get("/", (req, res) => res.send("Hello, World!"));
+  router.get("/intro", (req, res) => res.send("Nice to meet you."));
+  router.get("/goodbye", (req, res) => res.send("Goodbye!"));
+};
