73634803c34e4650bc388a5de75eb19b77ac5d75
Edson David Puente Guerrero <edpg_@hotmail.com>
2021-07-12 13:59:27 -0600

    fix(productos-compras): Correccion suma

--- extensions/endpoints/productos-compras/index.js
+++ extensions/endpoints/productos-compras/index.js
@@ -10,10 +10,17 @@ module.exports = function registerEndpoint(
   router.post("/", async (req, res, next) => {
     const db = [];
     for (let i = 0; i < req.body.productos.length; i++) {
+      const datoSuma = await database
+        .table("productos_comprados")
+        .where("id", req.body.productos[i].id)
+        .select("cantidad_ingresada");
       const dato = await database
         .table("productos_comprados")
         .where("id", req.body.productos[i].id)
-        .update({ cantidad_ingresada: req.body.productos[i].cantidad });
+        .update({
+          cantidad_ingresada:
+            datoSuma[0].cantidad_ingresada + req.body.productos[i].cantidad,
+        });
       db.push(dato);
     }
     res.json(db);
